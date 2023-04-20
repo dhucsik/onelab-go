e67c6577e3ca2cf569fe2737cca699af29655459
dhucsik <diasgalikhanov@gmail.com>
2023-04-20 14:09:05 +0600

    added integr test for handler

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/golang/mock v1.4.4
 	github.com/labstack/echo/v4 v4.9.0
 	github.com/spf13/viper v1.15.0
+	github.com/stretchr/testify v1.8.1
 	github.com/swaggo/echo-swagger v1.4.0
 	github.com/swaggo/swag v1.8.12
 	golang.org/x/crypto v0.6.0
@@ -16,6 +17,7 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
@@ -36,6 +38,7 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
---
+++ transport/http/handler/book_test.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"practice/config"
+	"practice/service"
+	"practice/storage"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	getBookJSON = `{
+		"id": "2",
+		"title": "book2",
+		"author": "author2",
+		"rent_price": 0
+	}`
+)
+
+func TestBookHandler_Get(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/api/v1/book", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	h, err := getBookHandler(t)
+	if err != nil {
+		t.Errorf("couldn't run server")
+		return
+	}
+
+	if assert.NoError(t, h.GetBook(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, getBookJSON, rec.Body.String())
+	}
+}
+
+func getBookHandler(t *testing.T) (*Manager, error) {
+	cfg, err := config.New()
+	if err != nil {
+		return nil, err
+	}
+
+	repo, err := storage.New(context.Background(), cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	srvManager, err := service.NewManager(repo)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewManager(srvManager), nil
+}
